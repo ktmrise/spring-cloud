067f8a63a5d0313bd6c97469326f07316e72731f
ktmrise <k2413114403@163.com>
2020-04-19 13:57:34 +0800

    save

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/ktm/springcloud/StreamMQMain8803.java
@@ -0,0 +1,18 @@
+package com.ktm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @Author: rise
+ * @create: 2020-04-18 17:04
+ **/
+
+
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
